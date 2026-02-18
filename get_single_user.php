f8604bc0070d927de5ac8885cee2ff5f943464a3
samah abozaid <samah.abozaid89@gmail.com>
2026-02-18 10:20:07 +0000

    étape 3 avec des commetaire

--- get_single_user.php
+++ get_single_user.php
@@ -2,18 +2,18 @@
 require './connexion.php'; // pour accéder à la base de données
 
 // Récupérer et sécuriser l'id
-$id = (int) $_GET['id']; // conversion en entier pour éviter les injections
+$id =[
+    'id'=> $_GET['id']
+    ]; 
 
 // Préparer la requête
 $query = $db->prepare("SELECT * FROM users WHERE id = :id");
 
-// Exécuter avec un tableau associatif
-$query->execute(['id' => $id]);
+$query->execute( $id);
 
 // Récupérer le résultat
 $user = $query->fetch(PDO::FETCH_ASSOC);
 
-// Afficher le résultat pour debug
-echo "<pre>";
 var_dump($user);
-echo "</pre>";
+
+?>
