011aecd5c40f33250cedfe3d298429cf1a3b7c03
samah abozaid <samah.abozaid89@gmail.com>
2026-02-18 14:35:01 +0000

    étape 8 : delete address

--- delete-address.php
+++ delete-address.php
@@ -3,18 +3,18 @@ require './connexion.php'; // connexion à la base de données
 
 //  Récupérer et sécuriser les valeurs
 
-$id      = (int) $_POST['id'];
+$id      = $_POST['id'];
 
 // Préparer la requête SQL pour mettre à jour l'adresse
-$query = $db->prepare("
-    DELEAT FROM adresse 
-    WHERE id = :id
-");
+$query = $db->prepare("DELETE FROM address WHERE id = :id");
 
-// Exécuter la requête avec les paramètres
-$query->execute([
-    'id'=> $id
-]);
+$parameters = [
+    'id' => $id
+];
+$query->execute($parameters);
+
 
 // Confirmation
-echo "Adresse modifiée avec succès !";
+echo "Adresse supprimer avec succès !";
+
+?>
